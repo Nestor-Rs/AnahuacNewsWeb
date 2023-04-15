0c531c054d0fedff785ae4ef032a9c0e3b1c04f7
Nestor-Rs <nestor.rodriguezsa@anahuac.mx>
2023-04-15 18:47:55 -0500

    Implementacion de Firestore

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
+import { app } from "./firebase.js";
+import { getFirestore,collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
 
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+const q = query(collection(db, "Publicaciones"));
+
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+});
