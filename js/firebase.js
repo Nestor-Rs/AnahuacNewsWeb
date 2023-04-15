15746c15d4ebf8033d1d182584022d4cffbd1d57
Nestor-Rs <nestor.rodriguezsa@anahuac.mx>
2023-04-15 15:36:08 -0500

    Implementacion de firebase

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+    
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwt73jMg3A6pO2tFz9a7OEeecn_rGNI3E",
+  authDomain: "anahuacnews-e419f.firebaseapp.com",
+  projectId: "anahuacnews-e419f",
+  storageBucket: "anahuacnews-e419f.appspot.com",
+  messagingSenderId: "893247985958",
+  appId: "1:893247985958:web:d95bde8b0b5d8ab56149e7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
