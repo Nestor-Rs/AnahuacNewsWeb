7bc6637ab0cd529fa81876a9284ec5e793000bc4
Nestor-Rs <nestor.rodriguezsa@anahuac.mx>
2023-05-11 19:07:55 -0500

    Update

--- src/js/publicacion.js
+++ src/js/publicacion.js
@@ -1,8 +1,9 @@
 export class publicacion{
-    constructor(id,titulo,texto,img){
+    constructor(id,titulo,texto,img,escuela){
         this.id=id;
         this.titulo=titulo;
         this.texto=texto;
         this.img=img;
+        this.escuela=escuela;
     }
 };
--- src/js/update.js
+++ src/js/update.js
@@ -1,5 +1,8 @@
 import { doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
 
-function  updatePub(db,data,) {
-    
+await export function  updatePub(db,object,id) {
+    const docRef = doc(db, objects, id);
+    const updateTimestamp = updateDoc(docRef, {
+        timestamp: serverTimestamp()
+    });
 }
